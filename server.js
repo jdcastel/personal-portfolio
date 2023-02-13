c2f9a5ae24f1a7b7dabe8d982ba4a5b04dea80fd
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2023-02-13 14:46:36 -0500

    server contact fixed

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "jdrcastel@gmail.com",
+    pass: "Ju@nd@21"
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "jdrcastel@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
